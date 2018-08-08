723aaf18bbaa8cfa319c3f927c736faeccd23317
BW Tech <30957824+bwtechyt@users.noreply.github.com>
2018-08-08 19:07:06 +0200

    Update credits.js

--- Commands/credits.js
+++ Commands/credits.js
@@ -3,26 +3,18 @@ const Discord = require("discord.js");
 exports.run = (client, msg, args) => {
 
     msg.channel.send({embed: {
-        color: 0xd45555,
+        color: 0xDCA741,
         author: {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
         description: `:hammer: **Project Lead**: BW Tech#1434`,
         fields: [{
-            name: "APIs",
-            value: `[Random Cat](http://random.cat) \n[Random Dog](http://random.dog)`,
-           inline: true
-          },
-          {
-            name: "Developers",
-            value: `Casbas 234#5853 and Hydrogen#0002`,
+            name: "Helpers",
+            value: `Casbas 234#5853 and Hydrogen#0002 (developers), TEC#0001 (bot hoster)`,
             inline: true
           },
         ],
-        footer: {
-          text: `Special thanks to TEC#0001 for hosting this bot!`
-            }
         }
     });
     console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
